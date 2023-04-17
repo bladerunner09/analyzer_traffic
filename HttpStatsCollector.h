2d41351707c8ad33fe4059e3a935032b3cdb2f1a
Daniil <daniil@Enigma.myguest.virtualbox.org>
2023-04-17 20:58:35 +0300

    Release build 0.0

--- HttpStatsCollector.h
+++ HttpStatsCollector.h
@@ -10,9 +10,6 @@
 #include "SystemUtils.h"
 
 
-/**
- * A struct for collecting stats on all HTTP requests
- */
 struct HttpRequestStats
 {
 	std::map<std::string, int> outDataLenghtPerHost;
@@ -20,9 +17,6 @@ struct HttpRequestStats
 };
 
 
-/**
- * A struct for collecting stats on all HTTP responses
- */
 struct HttpResponseStats
 {
 	std::map<std::string, int> inDataLenghtPerHost;
@@ -30,47 +24,33 @@ struct HttpResponseStats
 };
 
 
-/**
- * The HTTP stats collector. Should be called for every packet arriving and also periodically to calculate rates
- */
 class HttpStatsCollector
 {
 public:
 
-	/**
-	 * C'tor - clear all structures
-	 */
 	explicit HttpStatsCollector(uint16_t dstPort)
 	{
 		m_DstPort = dstPort;
 	}
 
-	/**
-	 * Collect stats for a single packet
-	 */
 	void collectStats(pcpp::Packet* httpPacket)
 	{
-		// verify packet is TCP
 		if (!httpPacket->isPacketOfType(pcpp::TCP))
 			return;
 
-		// verify packet is port 80
 		pcpp::TcpLayer* tcpLayer = httpPacket->getLayerOfType<pcpp::TcpLayer>();
 		if (!(tcpLayer->getDstPort() == m_DstPort || tcpLayer->getSrcPort() == m_DstPort))
 			return;
 
 		int dataSize = 0;
 
-		// collect general HTTP traffic stats on this packet
 		uint32_t hashVal = collectHttpTrafficStats(httpPacket, &dataSize);
 
-		// if packet is an HTTP request - collect HTTP request stats on this packet
 		if (httpPacket->isPacketOfType(pcpp::HTTPRequest))
 		{
 			pcpp::HttpRequestLayer* req = httpPacket->getLayerOfType<pcpp::HttpRequestLayer>();
 			collectRequestStats(req, dataSize);
 		}
-		// if packet is an HTTP response - collect HTTP response stats on this packet
 		else if (httpPacket->isPacketOfType(pcpp::HTTPResponse))
 		{
 			pcpp::HttpResponseLayer* res = httpPacket->getLayerOfType<pcpp::HttpResponseLayer>();
@@ -78,43 +58,27 @@ public:
 		}
 	}
 
-	/**
-	 * Get HTTP request stats
-	 */
 	HttpRequestStats& getRequestStats() { return m_RequestStats; }
-
-	/**
-	 * Get HTTP response stats
-	 */
 	HttpResponseStats& getResponseStats() { return m_ResponseStats; }
 
 private:
 
 	std::string lastRequestHost = "";
 
-	/**
-	 * Collect stats relevant for every HTTP packet (request, response or any other)
-	 * This method calculates and returns the flow key for this packet
-	 */
 	uint32_t collectHttpTrafficStats(pcpp::Packet* httpPacket, int* dataSize)
 	{
 		pcpp::TcpLayer* tcpLayer = httpPacket->getLayerOfType<pcpp::TcpLayer>();
 
 		*dataSize = tcpLayer->getLayerPayloadSize();
 
-		// calculate a hash key for this flow to be used in the flow table
 		uint32_t hashVal = pcpp::hash5Tuple(httpPacket);
 
 		return hashVal;
 	}
 
 
-	/**
-	 * Collect stats relevant for HTTP request messages
-	 */
 	void collectRequestStats(pcpp::HttpRequestLayer* req, int dataSize)
 	{
-		// extract hostname and add to hostname count map
 		pcpp::HeaderField* hostField = req->getFieldByName(PCPP_HTTP_HOST_FIELD);
 
 		m_RequestStats.outDataLenghtPerHost[hostField->getFieldValue()] += dataSize;
@@ -124,9 +88,6 @@ private:
 	}
 
 
-	/**
-	 * Collect stats relevant for HTTP response messages
-	 */
 	void collectResponseStats(pcpp::HttpResponseLayer* res, int dataSize)
 	{
 		m_ResponseStats.inDataLenghtPerHost[lastRequestHost] += dataSize;
--- main.cpp
+++ main.cpp
@@ -12,13 +12,7 @@
 #include <getopt.h>
 #include <iostream>
 #include <sstream>
-
-
-#define EXIT_WITH_ERROR(reason) do { \
-	printUsage(); \
-	std::cout << std::endl << "ERROR: " << reason << std::endl << std::endl; \
-	exit(1); \
-	} while(0)
+#include <glog/logging.h>
 
 #define DEFAULT_CALC_RATES_PERIOD_SEC 5
 
@@ -39,13 +33,10 @@ struct HttpPacketArrivedData
 };
 
 
-/**
- * Print application usage
- */
 void printUsage()
 {
 	std::cout << std::endl
-		<< "Usage: Live traffic mode:" << std::endl
+		<< "Traffic analyzer:" << std::endl
 		<< "-------------------------" << std::endl
 		<< pcpp::AppName::get() << " [-h] [-r calc_period] [-p dst_port] [-i interface]" << std::endl
 		<< std::endl
@@ -53,30 +44,22 @@ void printUsage()
 		<< std::endl
 		<< "    -i interface   : Use the specified interface. Can be interface name (e.g eth0) or interface IPv4 address" << std::endl
 		<< "    -p dst_port    : Use the specified port (optional parameter, the default is 80)" << std::endl
-		<< "    -r calc_period : The period in seconds to calculate rates. If not provided default is 2 seconds" << std::endl
+		<< "    -r calc_period : The period in seconds to calculate rates. If not provided default is 5 seconds" << std::endl
 		<< "    -h             : Displays this help message and exits" << std::endl
 		<< std::endl;
 }
 
 
-/**
- * packet capture callback - called whenever a packet arrives
- */
 void httpPacketArrive(pcpp::RawPacket* packet, pcpp::PcapLiveDevice* dev, void* cookie)
 {
-	// parse the packet
 	pcpp::Packet parsedPacket(packet);
 
 	HttpPacketArrivedData* data  = (HttpPacketArrivedData*)cookie;
 
-	// give the packet to the collector
 	data->statsCollector->collectStats(&parsedPacket);
 }
 
 
-/**
- * Print the summary traffic.
- */
 void printSummaryTraffic(HttpStatsCollector& collector)
 {
 	for (auto it = collector.getRequestStats().outDataLenghtPerHost.begin(); it != collector.getRequestStats().outDataLenghtPerHost.end(); it++) {
@@ -92,9 +75,6 @@ void printSummaryTraffic(HttpStatsCollector& collector)
 }
 
 
-/**
- * The callback to be called when application is terminated by ctrl-c. Stops the endless while loop
- */
 void onApplicationInterrupted(void* cookie)
 {
 	bool* shouldStop = (bool*)cookie;
@@ -102,29 +82,26 @@ void onApplicationInterrupted(void* cookie)
 }
 
 
-/**
- * activate HTTP analysis from live traffic
- */
 void analyzeHttpFromLiveTraffic(pcpp::PcapLiveDevice* dev, int printRatePeriod, uint16_t dstPort)
 {
-	// open the device
 	if (!dev->open())
-		EXIT_WITH_ERROR("Could not open the device");
+		LOG(FATAL) << "Could not open the device";
 
 	pcpp::PortFilter httpPortFilter(dstPort, pcpp::SRC_OR_DST);
 	if (!dev->setFilter(httpPortFilter))
-		EXIT_WITH_ERROR("Could not set up filter on device");
+		LOG(FATAL) << "Could not set up filter on device";
 
-	// start capturing packets and collecting stats
 	HttpPacketArrivedData data;
 	HttpStatsCollector collector(dstPort);
 	data.statsCollector = &collector;
 	dev->startCapture(httpPacketArrive, &data);
 
 
-	// register the on app close event to print summary stats on app termination
 	bool shouldStop = false;
 	pcpp::ApplicationEventHandler::getInstance().onApplicationInterrupted(onApplicationInterrupted, &shouldStop);
+
+	if (shouldStop == true)
+		LOG(DFATAL) << "Shoud be at least one try";
 
 	while(!shouldStop)
 	{
@@ -133,16 +110,18 @@ void analyzeHttpFromLiveTraffic(pcpp::PcapLiveDevice* dev, int printRatePeriod, 
 		printSummaryTraffic(collector);
 	}
 
-	// stop capturing and close the live device
 	dev->stopCapture();
 	dev->close();
+
+	LOG(INFO) << "End of execution";
 }
 
-/**
- * main method of this utility
- */
 int main(int argc, char* argv[])
 {
+	FLAGS_log_dir = "./";
+
+	google::InitGoogleLogging(argv[0]);
+
 	pcpp::AppName::init(argc, argv);
 
 	std::string interfaceNameOrIP = "";
@@ -152,6 +131,8 @@ int main(int argc, char* argv[])
 
 	int optionIndex = 0;
 	int opt = 0;
+
+	LOG(INFO) << "Start of execution";
 
 	while((opt = getopt_long(argc, argv, "i:p:r:h", HttpAnalyzerOptions, &optionIndex)) != -1)
 	{
@@ -174,26 +155,26 @@ int main(int argc, char* argv[])
 				break;
 			default:
 				printUsage();
+				LOG(ERROR) << "Unknown opt: " << opt;
 				exit(-1);
 		}
 	}
 
-	// if no interface - exit with error
 	if (interfaceNameOrIP == "")
-		EXIT_WITH_ERROR("Neither interface nor input pcap file were provided");
+		LOG(FATAL) << "Neither interface nor input pcap file were provided";
 
-	// get the port
 	int nPort = atoi(port.c_str());
 	if (nPort <= 0 || nPort > 65535)
-		EXIT_WITH_ERROR("Please input a number between 0 to 65535");
+		LOG(FATAL) << "Please input a number between 0 to 65535";
 
-	// analyze in live traffic mode
+	if (nPort == 443)
+		LOG(WARNING) << "Can't track HTTPS traffic";	
 
 	pcpp::PcapLiveDevice* dev = pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDeviceByIpOrName(interfaceNameOrIP);
 	if (dev == nullptr)
-		EXIT_WITH_ERROR("Couldn't find interface by provided IP address or name");
+		LOG(FATAL) << "Couldn't find interface by provided IP address or name";
 
-	// start capturing and analyzing traffic
 	analyzeHttpFromLiveTraffic(dev, printRatePeriod, nPort);
 
+	google::ShutdownGoogleLogging();
 }
